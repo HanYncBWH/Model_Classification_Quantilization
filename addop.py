e20680712449a6ad34f327649be7a6fd43e57b07
HanYçBH <126852923+HanYncBWH@users.noreply.github.com>
2024-09-20 11:46:31 +0800

    Update addop.py

--- addop.py
+++ addop.py
@@ -8,15 +8,14 @@ def preprocess(input_file):
     from PIL import Image
     image = Image.open(input_file)
     image = image.convert("RGB")
-    img_data = np.array(image.resize((224, 224), Image.BILINEAR)) / 255
+    img_data = np.array(image.resize((160, 160), Image.BILINEAR)) / 255
     img_data = np.transpose(img_data, (2, 0, 1))
     img_data = np.expand_dims(img_data, 0)
     return img_data
 
 input_data = preprocess(image_path)
 
-
-input_names = ["input_name"]
+input_names = ["input.1"]
 hw_results = ktc.kneron_inference([input_data], nef_file=nef_model_path, input_names=input_names, platform=720)
 
 
